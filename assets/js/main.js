13ef6df4f5c7da50e371232b58f3903d74a964d0
binh234 <57580923+binh234@users.noreply.github.com>
2021-05-07 16:57:20 +0700

    Remove unneccessary file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
- * Template Name: VinFast - v2.2.1
- * Template URL: https://bootstrapmade.com/VinFast-free-multipurpose-bootstrap-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 !(function($) {
     "use strict";
 
@@ -130,61 +124,6 @@
             scrollTop: 0
         }, 1500, 'easeInOutExpo');
         return false;
-    });
-
-    // Clients carousel (uses the Owl Carousel library)
-    $(".clients-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            768: {
-                items: 4
-            },
-            900: {
-                items: 6
-            }
-        }
-    });
-
-    // jQuery counterUp
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-    // Skills section
-    $('.skills-content').waypoint(function() {
-        $('.progress .progress-bar').each(function() {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {
-        offset: '80%'
-    });
-
-    // Porfolio isotope and filter
-    $(window).on('load', function() {
-        var portfolioIsotope = $('.portfolio-container').isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows'
-        });
-
-        $('#portfolio-flters li').on('click', function() {
-            $("#portfolio-flters li").removeClass('filter-active');
-            $(this).addClass('filter-active');
-
-            portfolioIsotope.isotope({
-                filter: $(this).data('filter')
-            });
-        });
-
-        // Initiate venobox (lightbox feature used in portofilo)
-        $(document).ready(function() {
-            $('.venobox').venobox();
-        });
     });
 
     // Portfolio details carousel
