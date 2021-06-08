469c98a1b80cbc390e8e4c543e7c127ace5d770f
suphan081 <dinhsu081@gmail.com>
2021-06-08 23:14:03 +0700

    fix title for customer

--- admin/customer.php
+++ admin/customer.php
@@ -49,7 +49,7 @@ session_start();
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Thông tin người dùng</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
